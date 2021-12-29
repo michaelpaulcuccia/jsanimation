6cf87379acd34dc508b57eb8e1d05055c43ca0ea
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2021-12-29 12:11:48 -0500

    some animations set up

--- chapter_2/project/app.js
+++ chapter_2/project/app.js
@@ -1,29 +1,37 @@
-// gsap.to(".text", {
-//   y: 200,
-//   opacity: 0,
-//   duration: 5,
-// });
+const tl = gsap.timeline({
+  defaults: {
+    duration: 0.75,
+    ease: "power1.out",
+  },
+});
 
-// gsap.fromTo(
-//   ".text",
-//   {
-//     opacity: 0,
-//   },
-//   {
-//     opacity: 1,
-//     duration: 10,
-//   }
-// );
-
-gsap.fromTo(
-  ".text",
+tl.from(
+  ".cookie-container",
   {
-    opacity: 0,
-    y: 20,
+    scale: 0,
   },
   {
-    opacity: 1,
-    y: 0,
-    duration: 1,
+    scale: 1,
+    //ease: "elastic.out(1, 0.4)",
   }
+);
+
+tl.fromTo(
+  ".cookie",
+  { opacity: 0, x: -50, rotation: "-45deg" },
+  { opacity: 1, x: 0, rotation: "0deg" }
+);
+
+tl.fromTo(
+  ".text",
+  {
+    x: 30,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+  },
+  //syncs with previous animation
+  "<"
 );
