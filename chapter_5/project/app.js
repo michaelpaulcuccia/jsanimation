afa7f0b6cfb76b5e2d3d89e29883928fa6d2517e
Michael Cuccia <michael.cuccia@egplusww.com>
2022-01-03 10:43:40 -0500

    bounce effect

---
+++ chapter_5/project/app.js
@@ -0,0 +1,35 @@
+const containers = document.querySelectorAll('.input-container');
+
+const form = document.querySelector('form');
+
+const tl = gsap.timeline({ defaults: { duration: 1} });
+
+//LINE ANIMATION
+
+const start = "M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512";
+
+const end = 'M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512';
+
+//ELASTIC EFFECT
+//loop through containers and see if input is focused
+
+containers.forEach(container => {
+    const input = container.querySelector('.input');
+    const line = container.querySelector('.elastic-line');
+    const placeholder = container.querySelector('.placeholder');
+
+    //focus - no two elements can be focused at same time
+    input.addEventListener('focus', () => {
+        //check if there is any text in input
+        if(!input.value) {
+            tl.fromTo(line, 
+                {attr: {d: start}},
+                {attr: {d: end}, ease: 'Power2.easeOut', duration: .75},
+            );
+            tl.to(line, 
+                //'<50%' means run halfway through previous animation
+                {attr: {d: start}, ease: 'elastic.out(3, .5)'}, '<50%'
+            )
+        }
+    })
+})
