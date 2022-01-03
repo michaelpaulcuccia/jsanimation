1f9e4510e9c8f3e722fa8c3760a2bed4744f4431
Michael Cuccia <michael.cuccia@egplusww.com>
2022-01-03 12:39:12 -0500

    html form validation

--- chapter_5/project/app.js
+++ chapter_5/project/app.js
@@ -1,13 +1,9 @@
 const containers = document.querySelectorAll('.input-container');
-
 const form = document.querySelector('form');
-
 const tl = gsap.timeline({ defaults: { duration: 1} });
 
 //LINE ANIMATION
-
 const start = "M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 300 0.999512 300 0.999512";
-
 const end = 'M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512';
 
 //ELASTIC EFFECT
